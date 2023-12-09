7e478de4ccc2a89f52ad50f764864efe87b42fac
Richard Dyer <Richard@r7development.com>
2023-12-09 08:24:34 -0700

    Day 8

--- tasks/2023/day9.go
+++ tasks/2023/day9.go
@@ -15,9 +15,29 @@ func init() {
 
 func (m Day9) Task1(data *string) (*string, error) {
 	lines := strings.Split(*data, "\n")
-	result := strconv.Itoa(len(lines))
+	lastNumberSum := 0
+	for _, line := range lines[0:1] {
+		numList := convertToNumList(line)
+		lastDifference := calculateLastDifference(numList)
+		lastNumberSum += numList[len(numList)-1] + lastDifference
+	}
+	result := strconv.Itoa(lastNumberSum)
 
 	return &result, nil
+}
+
+func calculateLastDifference(list []int) int {
+	returnInt := 0
+	return returnInt
+}
+
+func convertToNumList(line string) []int {
+	var returnList []int
+	for _, numString := range strings.Fields(line) {
+		num, _ := strconv.Atoi(numString)
+		returnList = append(returnList, num)
+	}
+	return returnList
 }
 
 func (m Day9) Task2(data *string) (*string, error) {
@@ -27,5 +47,9 @@ func (m Day9) Task2(data *string) (*string, error) {
 }
 
 func (m Day9) getTestData() []string {
-	return []string{}
+	return []string{
+		"0 3 6 9 12 15",
+		"1 3 6 10 15 21",
+		"10 13 16 21 30 45",
+	}
 }
